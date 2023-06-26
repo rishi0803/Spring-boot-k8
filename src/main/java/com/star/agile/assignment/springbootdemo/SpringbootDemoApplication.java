07e582e689b2fc469ca83ff97cf15c9f3f81d0fc
Rishi <rushikesh.asjadhav8@gmail.com>
2023-06-26 17:32:07 +0530

    update project

--- src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDemoApplication {
-
+    @GetMapping("/message")
+    public String displayMessage(){
+	  return "Congrulation you successfully deployed your application to kubernetes";  
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
